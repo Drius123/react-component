92aca399f003f9aff28066296442f329bb965ac6
Drius123 <adovgulev111@gmail.com>
2023-11-02 18:52:08 +0300

    fix: change spinner to func comp

--- src/components/Spiner/Spiner.tsx
+++ src/components/Spiner/Spiner.tsx
@@ -1,14 +1,9 @@
-import { Component, ReactNode } from 'react';
 import styles from './Spiner.module.scss';
 
-class Spinner extends Component {
-  render(): ReactNode {
-    return (
-      <div className={styles['spin-wrapper']}>
-        <div className={styles.spinner} />
-      </div>
-    );
-  }
+export default function Spinner() {
+  return (
+    <div className={styles['spin-wrapper']}>
+      <div className={styles.spinner} />
+    </div>
+  );
 }
-
-export default Spinner;

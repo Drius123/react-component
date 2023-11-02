9ca702bda32c77b2e955b87d16c07ec169a0f94b
Drius123 <adovgulev111@gmail.com>
2023-11-02 20:37:34 +0300

    feat: add search

--- src/components/SearchInput/SearchInput.tsx
+++ src/components/SearchInput/SearchInput.tsx
@@ -1,7 +1,11 @@
+import { useState } from 'react';
 import search from '../../assets/search-icon.svg';
+import { SearchInputProps } from '../../types';
 import styles from './SearchInput.module.scss';
+import { getItem } from '../../services/getSomething';
 
-export default function SearchInput() {
+export default function SearchInput({ changeLoading, changeState }: SearchInputProps) {
+  const [value, setValue] = useState('');
   return (
     <div>
       <h1 className={styles.title}>Start search your Star Wars Hero!</h1>
@@ -10,12 +14,19 @@ export default function SearchInput() {
         action=""
         onSubmit={(e) => {
           e.preventDefault();
+          changeLoading(true);
+          getItem(value).then((res) => (changeState(res), changeLoading(false)));
         }}
       >
         <button className={styles['search-btn']} type="submit">
           <img src={search} alt="search-icon" />
         </button>
-        <input className={styles.input} type="search" placeholder="Поиск" />
+        <input
+          className={styles.input}
+          type="search"
+          placeholder="Поиск"
+          onChange={(e) => setValue(e.currentTarget.value)}
+        />
       </form>
     </div>
   );
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,17 +1,29 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Presentation from '../../components/PresentationSection/Presentation';
 import SearchInput from '../../components/SearchInput/SearchInput';
 import styles from './HomePage.module.scss';
-import { getPeople } from '../../services/getSomething';
+import { getPeopleByPage } from '../../services/getSomething';
+import Spinner from '../../components/Spiner/Spiner';
+import { Results } from '../../types';
 
 export default function HomePage() {
-  const [result, setResult] = useState([]);
-  getPeople().then((res) => setResult(res));
+  const [result, setResult] = useState<Results>([]);
+  const [resultPeople, setResultPeople] = useState<Results>([]);
+  const [page, setPage] = useState(0);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    getPeopleByPage(1).then(
+      (res: Results) => (setResultPeople(res), setResult(res), setLoading(false))
+    );
+  }, []);
   return (
     <div className={styles.content}>
       <div className={styles['header-content']}>
-        <SearchInput />
-        <Presentation items={result} />
+        <SearchInput changeLoading={setLoading} changeState={setResult} />
+        <button className={styles.btn} onClick={() => setResult(resultPeople)}>
+          Show All Star Wars Hero
+        </button>
+        {loading ? <Spinner /> : <Presentation items={result} />}
       </div>
     </div>
   );

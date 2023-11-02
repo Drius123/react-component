89336263c5825e34a2017ab86b2bbc8c8b69a81f
Drius123 <adovgulev111@gmail.com>
2023-11-02 18:58:20 +0300

    feat: change notfoundpage to func comp

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,16 +1,11 @@
 import styles from './NotFoundPage.module.scss';
 
 import error404 from '../../assets/error-404.png';
-import { Component, ReactNode } from 'react';
 
-class NotFoundPage extends Component {
-  render(): ReactNode {
-    return (
-      <div className={styles.container}>
-        <img src={error404} alt="error-404-img" />
-      </div>
-    );
-  }
+export default function NotFoundPage() {
+  return (
+    <div className={styles.container}>
+      <img src={error404} alt="error-404-img" />
+    </div>
+  );
 }
-
-export default NotFoundPage;
